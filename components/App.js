8993fe3dcc5ff248b578e289de064997b732b39b
rachelsleet <rachelsleet@gmail.com>
2019-05-11 19:47:48 +0200

    create all required elements and align centre

--- components/App.js
+++ components/App.js
@@ -4,9 +4,37 @@ import Button from 'react-bootstrap/Button';
 class App extends Component {
   render() {
     return (
-      <div>
+      <div style={{ textAlign: 'center' }}>
         <h1>Pomodora Clock</h1>
-        <Button variant='light'>Button</Button>
+        <div id='container'>
+          <div class='row'>
+            <div class='col-sm' id='break-container'>
+              <div id='break-label'>Break Length</div>
+              <Button id='break-increment' variant='light'>
+                +
+              </Button>
+              <Button id='break-decrement' variant='light'>
+                -
+              </Button>
+              <div id='break-length'>5</div>
+            </div>
+            <div class='col-sm' id='session-container'>
+              <div id='session-label'>Session Length</div>
+              <Button id='session-increment' variant='light'>
+                +
+              </Button>
+              <Button id='session-decrement' variant='light'>
+                -
+              </Button>{' '}
+              <div id='session-length'>25</div>
+            </div>
+          </div>
+        </div>
+
+        <div id='timer-label'>Session</div>
+        <div id='time-left'>25:00</div>
+        <Button id='start_stop'>Start</Button>
+        <Button id='reset'>Reset</Button>
       </div>
     );
   }
