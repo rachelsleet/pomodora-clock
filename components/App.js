008389572625a6a7d9f2fe6b52b8d6388248092a
rachelsleet <rachelsleet@gmail.com>
2019-05-11 17:36:49 +0200

    set up with react, webpack, babel, bootstrap

---
+++ components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Pomodora Clock</h1>
+        <Button variant='light'>Button</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
